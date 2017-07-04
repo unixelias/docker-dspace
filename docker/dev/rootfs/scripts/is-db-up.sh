268adee695318a2e1cc5ba9da02a79808c33a800
Elias Alves <unixelias@gmail.com>
2017-07-04 02:16:00 -0300

    Linhk db host

--- docker/dev/rootfs/scripts/is-db-up.sh
+++ docker/dev/rootfs/scripts/is-db-up.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-until sudo -u postgres psql -c '\l'; do
+until sudo -u postgres psql -h 127.0.0.1 -p 5432 -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
