c977993e6c45ab8d323785955384af42a3df2363
Elias Alves <unixelias@users.noreply.github.com>
2017-07-08 00:24:34 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 shopt -s extglob
 
-# for i in $( ls docker); do
-#   if [ "$i" == "postgres" ]; then
-#     ### Build DB
-#     docker build -t unixelias/postgres-dspace:9.6-dev \
-#         --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-#         --build-arg VCS_REF=`git rev-parse --short HEAD` \
-#         --build-arg VERSION="9.6-dev" docker/postgres \
-#         && docker push unixelias/postgres-dspace:9.6-dev
-#   else
-#     case "$i" in
-#         *-test )  ;;
-#         * ) docker build -t unixelias/docker-dspace:$i \
-#             --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-#             --build-arg VCS_REF=`git rev-parse --short HEAD` \
-#             --build-arg VERSION="$i" docker/$i \
-#             && docker push unixelias/docker-dspace:$i ;;
-#     esac
-#   fi
-# done
-
-### Build DB
-docker build -t unixelias/postgres-dspace:9.6-dev \
+for i in $( ls docker); do
+  if [ "$i" == "postgres" ]; then
+    ### Build DB
+    docker build -t unixelias/postgres-dspace:9.6-dev \
         --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
         --build-arg VCS_REF=`git rev-parse --short HEAD` \
-        --build-arg VERSION='9.6-dev' docker/postgres \
-   && docker push unixelias/postgres-dspace:9.6-dev
+        --build-arg VERSION="9.6-dev" docker/postgres \
+        && docker push unixelias/postgres-dspace:9.6-dev
+  else
+    case "$i" in
+        *-test )  ;;
+        * ) docker build -t unixelias/docker-dspace:$i \
+            --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+            --build-arg VCS_REF=`git rev-parse --short HEAD` \
+            --build-arg VERSION="$i" docker/$i \
+            && docker push unixelias/docker-dspace:$i ;;
+    esac
+  fi
+done
+
+### Build DB
+#docker build -t unixelias/postgres-dspace:9.6-dev \
+#        --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+#        --build-arg VCS_REF=`git rev-parse --short HEAD` \
+#        --build-arg VERSION='9.6-dev' docker/postgres \
+#   && docker push unixelias/postgres-dspace:9.6-dev
 
 ### Build DSpace
 
-docker build -t unixelias/docker-dspace:6.0-dev \
-            --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-            --build-arg VCS_REF=`git rev-parse --short HEAD` \
-            --build-arg VERSION="6.0-dev" docker/dev \
-   && docker push unixelias/docker-dspace:6.0-dev
+#docker build -t unixelias/docker-dspace:6.0-dev \
+#            --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+#            --build-arg VCS_REF=`git rev-parse --short HEAD` \
+#            --build-arg VERSION="6.0-dev" docker/dev \
+#   && docker push unixelias/docker-dspace:6.0-dev
