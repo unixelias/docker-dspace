2694cf9878686b5b167abac4bd134fcaa0eb7fdf
Elias Alves <unixelias@gmail.com>
2018-02-22 21:39:41 -0300

    New tag 5.7-ibict
    Get code from new source adress

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ docker build -t unixelias/docker-dspace:${TRAVIS_BRANCH} \
     --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
     --build-arg VCS_BRANCH=${TRAVIS_BRANCH} \
     --build-arg VCS_REF=`git rev-parse --short HEAD` \
-    --build-arg DSPACE_VERSION=`git ls-remote --tags https://github.com/DSpace/DSpace.git | awk '{print $2}' | grep -v '{}' | egrep -e "dspace-?.?" | grep -v 'parent' | grep -v 'imscp' | awk -F"/" '{print $3}' | sort -V -t. -k1,1 -k2,2 -k3,3 | tail -n 1 | cut -d'-' -f 2`\
+    --build-arg SOURCE_REPO="https://github.com/ibict-br2/repositorio-padrao.git" \
+    --build-arg SOURCE_BRANCH="dspace-5_x" \
     --build-arg TOMCAT_MAJOR=`svn list http://svn.apache.org/repos/asf/tomcat/tags | sort -V -t. -k1,1 -k2,2 -k3,3 | tail -n 1 | cut -d '_' --fields=2` \
     --build-arg TOMCAT_VERSION=`svn list http://svn.apache.org/repos/asf/tomcat/tags | sort -V -t. -k1,1 -k2,2 -k3,3 | tail -n 1 | cut -d '_' --fields=2,3,4 | cut -d'/' -f 1 | tr -t '_' '.'` \
     --build-arg MAVEN_MAJOR=`git ls-remote --tags https://github.com/apache/maven.git | awk '{print $2}' | grep -v '{}' | grep -v 'workspace' | awk -F"/" '{print $3}' | sort -V -t. -k1,1 -k2,2 -k3,3 | tail -n 1 | cut -d'.' -f 1 | cut -d'-' -f2` \
